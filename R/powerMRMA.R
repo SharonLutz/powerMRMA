c9108ff948dfd3582a22291c554b2fe8d3c88a66
Annie Thwing <athwing@yahoo.com>
2018-05-05 22:51:13 -0600

    Fixed bug in filling in matrix

--- R/powerMRMA.R
+++ R/powerMRMA.R
@@ -1,7 +1,6 @@
-powerMRMA <-
-function(plot.name = "powerMRMAplot",MethodNames = c("MR.Classical","MR.Egger","MR.IVW","MR.Median","MA.Imai","MA.4Way"),n = 1000,n.sim=500,
+powerMRMA <-function(plot.name = "powerMRMAplot",MethodNames = c("MR.Classical","MR.Egger","MR.IVW","MR.Median","MA.Imai","MA.4Way"),n = 1000,n.sim=500,
                       MeasurementError=T, alpha.level=0.05, legend.include=T, color=T, SEED=1, nSNP = 4, MAF=c(0.2,0.2,0.2,0.2), varM = 1, varY = 1, gamma0 = 0,
-                      gammaX = c(.15,.15,.15,.15), gammaU = 0, beta0 = 0, betaM = c(0,0.2,0.25) , betaX = c(0,0,0,0), betaI = c(0,0,0,0), betaU = 0, 
+                      gammaX = c(.15,.15,.15,.15), gammaU = 0, beta0 = 0, betaM = c(0,0.2,0.25), betaX = c(0,0,0,0), betaI = c(0,0,0,0), betaU = 0, 
                       muU = 0, varU = 1, muME = 0, varME = 1){
   
   library(MendelianRandomization)
@@ -12,7 +11,7 @@ function(plot.name = "powerMRMAplot",MethodNames = c("MR.Classical","MR.Egger","
   possibleMethods <- c("MR.Classical","MR.Egger","MR.IVW","MR.Median","MA.Imai","MA.4Way")
   
   # Length of methods to be used for color/ line type/ symbol vectors
-  numMethods <- length(methodnames)
+  numMethods <- length(MethodNames)
   
   colorVec <- c("black" ,"red" , "green3" , "blue" , "cyan" , "magenta")
   
@@ -24,32 +23,32 @@ function(plot.name = "powerMRMAplot",MethodNames = c("MR.Classical","MR.Egger","
   }
   
   pchv <- c(1:numMethods)
-  ltyv <- rep_len(c(2:numMethods),length.out = length(methodnames))
+  ltyv <- rep_len(c(2:numMethods),length.out = length(MethodNames))
   
   # Check that the methods are entered correctly
-  for(mm in 1:length(methodnames)){
-    if(!toupper(methodnames[mm])%in%toupper(possibleMethods)){
-      stop(paste("Error: ",methodnames[mm]," is not a valid method name. See ?powerMRMA.",sep=""))
+  for(mm in 1:length(MethodNames)){
+    if(!toupper(MethodNames[mm])%in%toupper(possibleMethods)){
+      stop(paste("Error: ",MethodNames[mm]," is not a valid method name. See ?powerMRMA.",sep=""))
     }
   }
   
   ### Reorder and rename methods to be same order every time
   tempMethods <- rep(NA,length(possibleMethods))
   
-  for(jj in 1:length(methodnames)){
-    if(toupper(methodnames[jj])=="MR.CLASSICAL"){tempMethods[1]<-"MR.Classical"}
-    if(toupper(methodnames[jj])=="MR.EGGER"){tempMethods[2]<-"MR.Egger"}
-    if(toupper(methodnames[jj])=="MR.IVW" ){tempMethods[3]<-"MR.IVW"}
-    if(toupper(methodnames[jj])=="MR.MEDIAN" ){tempMethods[4]<-"MR.Median"}
-    if(toupper(methodnames[jj])=="MA.IMAI"){tempMethods[5]<-"MA.Imai"}
-    if(toupper(methodnames[jj])=="MA.4WAY"){tempMethods[6]<-"MA.4Way"}
+  for(jj in 1:length(MethodNames)){
+    if(toupper(MethodNames[jj])=="MR.CLASSICAL"){tempMethods[1]<-"MR.Classical"}
+    if(toupper(MethodNames[jj])=="MR.EGGER"){tempMethods[2]<-"MR.Egger"}
+    if(toupper(MethodNames[jj])=="MR.IVW" ){tempMethods[3]<-"MR.IVW"}
+    if(toupper(MethodNames[jj])=="MR.MEDIAN" ){tempMethods[4]<-"MR.Median"}
+    if(toupper(MethodNames[jj])=="MA.IMAI"){tempMethods[5]<-"MA.Imai"}
+    if(toupper(MethodNames[jj])=="MA.4WAY"){tempMethods[6]<-"MA.4Way"}
   }
   
-  methodnames <- tempMethods[!is.na(tempMethods)]
+  MethodNames <- tempMethods[!is.na(tempMethods)]
   ####
   
   # Error Checks for type/ length
-  if(nSNP!=length(MAF)){stop("Error: nSNP must equal length(MAF).")}
+if(nSNP!=length(MAF)){stop("Error: nSNP must equal length(MAF).")}
   if(nSNP!=length(gammaX)){stop("Error: nSNP must equal length(gammaX).")}
   if(nSNP!=length(betaX)){stop("Error: nSNP must equal length(betaX).")}
   if(nSNP!=length(betaI)){stop("Error: nSNP must equal length(betaI).")}
@@ -76,11 +75,10 @@ function(plot.name = "powerMRMAplot",MethodNames = c("MR.Classical","MR.Egger","
   for(ind in 1:n.sim){
     print(paste("sim ",ind," of ",n.sim,sep = ""))
     
+    mat <- matrix(0,nrow=length(betaM),ncol=6)
+    colnames(mat) <- c("MR.Classical","MR.Egger","MR.IVW","MR.Median","MA.Imai","MA.4Way")
+    
     for(betaM.ind in 1:length(betaM)){
-      
-      mat <- matrix(0,nrow=length(betaM),ncol=6)
-      colnames(mat) <- c("MR.Classical","MR.Egger","MR.IVW","MR.Median","MA.Imai","MA.4Way")
-      
       ## Data Generation
       matX <- matrix(0,nrow=n,ncol=nSNP)
       for(xx in 1:nSNP){
@@ -124,7 +122,7 @@ function(plot.name = "powerMRMAplot",MethodNames = c("MR.Classical","MR.Egger","
         betaXseMRy <- c(betaXseMRy,summary(fitY)$coefficients[2,2])
       }
       
-      if("MR.Classical" %in% methodnames){
+      if("MR.Classical" %in% MethodNames){
         fitY <- lm(Y~matX[,1])
         if(summary(fitY)$coefficients[2,4] < (alpha.level/nSNP)){ mat[betaM.ind,"MR.Classical"] <- mat[betaM.ind,"MR.Classical"] + 1 }
       }
@@ -132,20 +130,20 @@ function(plot.name = "powerMRMAplot",MethodNames = c("MR.Classical","MR.Egger","
       # Variants aren't correlated so don't need to include a correlation matrix
       mr.input <- mr_input(bx = betaXMR, bxse = betaXseMR, by = betaXMRy, byse = betaXseMRy)
       # Run all MR methods
-      if("MR.Median" %in% methodnames){ 
+      if("MR.Median" %in% MethodNames){ 
         mr.median.p <- mr_median(mr.input,seed = NA)$Pvalue
         if(mr.median.p < alpha.level){ mat[betaM.ind,"MR.Median"] <- mat[betaM.ind,"MR.Median"] + 1 }
       }
-      if("MR.Egger" %in% methodnames){
+      if("MR.Egger" %in% MethodNames){
         mr.egger.p <- mr_egger(mr.input,robust = F,penalized = F)$Pvalue.Est
         if(mr.egger.p < alpha.level){ mat[betaM.ind,"MR.Egger"] <- mat[betaM.ind,"MR.Egger"] + 1 }
       }
-      if("MR.IVW" %in% methodnames){
+      if("MR.IVW" %in% MethodNames){
         mr.ivw.p <- mr_ivw(mr.input,robust = F,penalized = F)$Pvalue
         if(mr.ivw.p < alpha.level){ mat[betaM.ind,"MR.IVW"] <- mat[betaM.ind,"MR.IVW"] + 1 }
       }
       
-      if("MA.Imai" %in% methodnames){
+      if("MA.Imai" %in% MethodNames){
         nSimImai <- 1000
         SNPinterest <- matX[,snp.ind]
         # Interaction between x1 and M on Y
@@ -156,7 +154,7 @@ function(plot.name = "powerMRMAplot",MethodNames = c("MR.Classical","MR.Egger","
         if(pindirect<(alpha.level/nSNP)){mat[betaM.ind,"MA.Imai"] <- mat[betaM.ind,"MA.Imai"]+1}
       }
       
-      if("MA.4Way" %in% methodnames){
+      if("MA.4Way" %in% MethodNames){
         SNPinterest <- matX[,snp.ind]
         medV.out <- mediationV(dataS=cbind(SNPinterest,M,Y),yvar="Y", mvar="M", avar="SNPinterest", yreg="linear", mreg="linear",int=TRUE,output="full",boot=T,alpha_level = alpha.level/nSNP)
         medV.out <- as.matrix(medV.out)
@@ -182,24 +180,24 @@ function(plot.name = "powerMRMAplot",MethodNames = c("MR.Classical","MR.Egger","
   
   plot(-1,-1,xlim=c(min(betaM),max(betaM)),ylim=c(0,1),ylab="Power",xlab="beta M")
   
-    for(mp in 1:length(methodnames)){
-      if(methodnames[mp]=="MR.Classical"){lines(betaM,mat_results[,"MR.Classical"],pch = pchv[mp],col = colorVec[mp],type="b",lty = ltyv[mp])}
-      if(methodnames[mp]=="MR.Egger"){lines(betaM,mat_results[,"MR.Egger"],pch = pchv[mp],col = colorVec[mp],type="b",lty = ltyv[mp])}
-      if(methodnames[mp]=="MR.IVW"){lines(betaM,mat_results[,"MR.IVW"],pch = pchv[mp],col = colorVec[mp],type="b",lty = ltyv[mp])}
-      if(methodnames[mp]=="MR.Median"){lines(betaM,mat_results[,"MR.Median"],pch = pchv[mp],col = colorVec[mp],type="b",lty = ltyv[mp])}
-      if(methodnames[mp]=="MA.Imai"){lines(betaM,mat_results[,"MA.Imai"],pch = pchv[mp],col = colorVec[mp],type="b",lty = ltyv[mp])}
-      if(methodnames[mp]=="MA.4Way"){lines(betaM,mat_results[,"MA.4Way"],pch = pchv[mp],col=colorVec[mp],type="b",lty = ltyv[mp])}
+    for(mp in 1:length(MethodNames)){
+      if(MethodNames[mp]=="MR.Classical"){lines(betaM,mat_results[,"MR.Classical"],pch = pchv[mp],col = colorVec[mp],type="b",lty = ltyv[mp])}
+      if(MethodNames[mp]=="MR.Egger"){lines(betaM,mat_results[,"MR.Egger"],pch = pchv[mp],col = colorVec[mp],type="b",lty = ltyv[mp])}
+      if(MethodNames[mp]=="MR.IVW"){lines(betaM,mat_results[,"MR.IVW"],pch = pchv[mp],col = colorVec[mp],type="b",lty = ltyv[mp])}
+      if(MethodNames[mp]=="MR.Median"){lines(betaM,mat_results[,"MR.Median"],pch = pchv[mp],col = colorVec[mp],type="b",lty = ltyv[mp])}
+      if(MethodNames[mp]=="MA.Imai"){lines(betaM,mat_results[,"MA.Imai"],pch = pchv[mp],col = colorVec[mp],type="b",lty = ltyv[mp])}
+      if(MethodNames[mp]=="MA.4Way"){lines(betaM,mat_results[,"MA.4Way"],pch = pchv[mp],col=colorVec[mp],type="b",lty = ltyv[mp])}
     }
   
   # Create a legend only out of the included methods
-  leg.text<-rep(0,length(methodnames))
-  for(ff in 1:length(methodnames)){
-    if(methodnames[ff]=="MR.Classical"){leg.text[ff]<-c("MR.Classical")}
-    if(methodnames[ff]=="MR.Egger"){leg.text[ff]<-c("MR.Egger")}
-    if(methodnames[ff]=="MR.IVW"){leg.text[ff]<-c("MR.IVW")}
-    if(methodnames[ff]=="MR.Median"){leg.text[ff]<-c("MR.Median")}
-    if(methodnames[ff]=="MA.Imai"){leg.text[ff]<-c("MA.Imai")}
-    if(methodnames[ff]=="MA.4Way"){leg.text[ff]<-c("MA.4Way")}
+  leg.text<-rep(0,length(MethodNames))
+  for(ff in 1:length(MethodNames)){
+    if(MethodNames[ff]=="MR.Classical"){leg.text[ff]<-c("MR.Classical")}
+    if(MethodNames[ff]=="MR.Egger"){leg.text[ff]<-c("MR.Egger")}
+    if(MethodNames[ff]=="MR.IVW"){leg.text[ff]<-c("MR.IVW")}
+    if(MethodNames[ff]=="MR.Median"){leg.text[ff]<-c("MR.Median")}
+    if(MethodNames[ff]=="MA.Imai"){leg.text[ff]<-c("MA.Imai")}
+    if(MethodNames[ff]=="MA.4Way"){leg.text[ff]<-c("MA.4Way")}
   }
   
   if(legend.include==TRUE){legend("topleft",legend = leg.text,col=colorVec,lty=ltyv,pch=pchv)}
